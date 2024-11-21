9503a515a76780ae8aae635caa2cacffab9f6d56
Emmanuel Cordoba <emmanuelc27@gmail.com>
2024-11-21 10:13:21 -0300

    Test para borrar un tag

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,4 @@ Route::get('/', function () {
 });
 
 Route::post('/tags', [ TagController::class, 'store' ]);
+Route::delete('/tags/{tag}', [ TagController::class, 'destroy' ]);
--- tests/Feature/Http/Controllers/TagControllerTest.php
+++ tests/Feature/Http/Controllers/TagControllerTest.php
@@ -2,18 +2,27 @@
 
 namespace Tests\Feature\Http\Controllers;
 
+use App\Models\Tag;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class TagControllerTest extends TestCase
 {
-    use WithFaker, RefreshDatabase;
+    use RefreshDatabase;
 
     public function test_store(): void
     {
         $this->post('/tags', ['name' => 'PHP'])
              ->assertRedirect('/');
         $this->assertDatabaseHas('tags', ['name' => 'PHP']);
+    }
+
+    public function test_delete(): void
+    {
+        $tag = Tag::factory()->create();
+        $this->delete("/tags/$tag->id")
+            ->assertRedirect('/');
+        $this->assertDatabaseMissing('tags', ['name' => $tag->name]);
     }
 }
